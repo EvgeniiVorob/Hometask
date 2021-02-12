bca81e1d808ab50a9136aae4bf8e3df7d5636c2f
EvgeniiVorob <78928209+EvgeniiVorob@users.noreply.github.com>
2021-02-12 18:21:29 +0400

    Update method_left_rectangle.java

--- src/Task1/method_left_rectangle.java
+++ src/Task1/method_left_rectangle.java
@@ -2,16 +2,14 @@ package Task1;
 
 public class method_left_rectangle {
 
-   public double InFunction(double x) {
-       return Math.sin(x);
-   }
+
     private double step = 0.1;
 
     public double integrate(argument x, double x1, double x2){
         double result = 0;
         double h = ((x2 - x1) / step);
         for(int i = 0; i < h; i++){
-            result = result + InFunction(x1 + step * i );
+            result = result + x.Function(x1 + step * (i + step));
         }
         result = result*step;
         return result ;
